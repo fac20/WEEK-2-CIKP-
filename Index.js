b7c661a170a0b6e3fc1fd5ca48dfa00a56f9950f
Terry Abdullah <glenroyterence@gmail.com>
2020-07-23 14:50:14 +0100

    Add search functionality to input field to fetch a gif

--- Index.js
+++ Index.js
@@ -1,6 +1,11 @@
 //link button to dom
 const jokeButton = document.querySelector("#joke-button");
 const jokeContainer = document.querySelector(".joke-text");
+let APIKEY = "ZbO2nno31mS8mT8mX8LL6XjUjYgZ9pvP";
+
+// selecting gif-container and creating an image element to house the Giphy gif 
+let img = document.querySelector(".gif-container");
+let image = document.createElement("img");
 
 //on click, fetch from url
 fetch("https://icanhazdadjoke.com/", {
@@ -19,11 +24,8 @@ fetch("https://icanhazdadjoke.com/", {
   /* next then statement has dataObj.joke as searchText due to
    previous return statement. It is then followed by  the fetch statement for the Giphy API */
   .then((searchText) => {
-    // selecting gif-container and creating an image element to house the Giphy gif 
-    let img = document.querySelector(".gif-container");
-    let image = document.createElement("img");
+    
 
-    let APIKEY = "ZbO2nno31mS8mT8mX8LL6XjUjYgZ9pvP";
     //fetch statement has searchText variable and APIKEY variable 
     fetch(
       `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=${searchText}`
@@ -42,6 +44,8 @@ fetch("https://icanhazdadjoke.com/", {
       .catch(console.error);
   })
   .catch((error) => console.log(error));
+    
+
 
 
 let happyButton = document.querySelector("#happy");
@@ -54,3 +58,33 @@ let unhappyButton = document.querySelector("#unhappy");
 const searchForm = document.querySelector(".search-form");
 
 unhappyButton.addEventListener("click", () => searchForm.classList.remove("hidden") );
+
+
+
+const submitButton = document.querySelector("#submit-button")
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let searchBarText = document.querySelector("#search-bar").value
+
+    //fetch statement has searchText variable and APIKEY variable 
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=${searchBarText}`
+    )
+    .then( response => response.json())
+    .then( (gif) => {
+      let giphyUrl = gif.data[0].images.downsized_large.url;
+      // assigning giphyUrl to image src 
+      image.src = giphyUrl;
+      //appending img to image tag
+      img.appendChild(image);
+      
+    })
+    .catch((error) => console.log(error));
+    // .catch(console.error);
+    // .catch((error) => console.log(error));
+
+
+})
+  
